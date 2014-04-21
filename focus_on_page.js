2316c9902101f33e5517a7145ce374170d205eb4
chibimagic <git@chibimagic.org>
2014-04-21 13:25:42 -0700

    Initial commit

---
+++ focus_on_page.js
@@ -0,0 +1,5 @@
+// gbqfq is id of the search input box
+document.getElementById('gbqfq').focus()
+document.getElementById('gbqfq').blur()
+// gsr is id of the body tag
+// document.getElementById('gsr').focus()
